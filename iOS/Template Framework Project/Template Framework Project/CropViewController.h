e03f5ef19d49df95fa95d6df32eeae522d653a04
Tim Liew <tltimmlw@gmail.com>
2016-11-02 15:05:44 -0700

    Started Box workflow to allow recruiters to choose an area to parse

---
+++ iOS/Template Framework Project/Template Framework Project/CropViewController.h
@@ -0,0 +1,13 @@
+//
+//  CropViewController.h
+//  Template Framework Project
+//
+//  Created by Tim Liew on 11/2/16.
+//  Copyright © 2016 Daniele Galiotto - www.g8production.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CropViewController : UIViewController
+
+@end
