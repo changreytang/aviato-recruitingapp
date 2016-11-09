e8885fcab506f2f7eb594cce953c1dbcb9707b47
Chang Rey Tang <tang.changrey@gmail.com>
2016-11-09 10:10:22 -0800

    Added new contact parsing files, basic bones groundwork

---
+++ iOS/Template Framework Project/Template Framework Project/ResumeContactParser.h
@@ -0,0 +1,17 @@
+//
+//  ResumeContactParser.h
+//  Template Framework Project
+//
+//  Created by Chang Rey Tang on 11/7/16.
+//  Copyright © 2016 Daniele Galiotto - www.g8production.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ResumeContactParser : NSObject
+
+@property (readonly, nonatomic) NSString* name;
+@property (readonly, nonatomic) NSString* email;
+@property (readonly, nonatomic) NSString* phone_number;
+
+@end
---
+++ iOS/Template Framework Project/Template Framework Project/ResumeContactParser.m
@@ -0,0 +1,26 @@
+//
+//  ResumeContactParser.m
+//  Template Framework Project
+//
+//  Created by Chang Rey Tang on 11/7/16.
+//  Copyright © 2016 Daniele Galiotto - www.g8production.com. All rights reserved.
+//
+
+#import "ResumeContactParser.h"
+
+@implementation ResumeContactParser
+
+- (void)parseContactInfo:(NSString*)contact_info {
+    
+}
+
+//- (IBAction)captureCropBox:(id)sender {
+//    UIGraphicsBeginImageContext(self.cropBoxView.frame.size);
+//    
+//    //NSLog(@"%@",self.cropBoxView.frame);
+//    [[self.resumeImageView layer] renderInContext:UIGraphicsGetCurrentContext()];
+//    UIImage *screenshot = UIGraphicsGetImageFromCurrentImageContext();
+//    [self.testImageView setImage:screenshot];
+//}
+
+@end
