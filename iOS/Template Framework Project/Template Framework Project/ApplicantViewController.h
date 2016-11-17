213510e6de20674754f46e82d598a294d506afe6
Tim Liew <tltimmlw@gmail.com>
2016-11-17 15:06:58 -0800

    Added ApplicantViewController class

---
+++ iOS/Template Framework Project/Template Framework Project/ApplicantViewController.h
@@ -0,0 +1,13 @@
+//
+//  ApplicantViewController.h
+//  Template Framework Project
+//
+//  Created by Tim Liew on 11/17/16.
+//  Copyright © 2016 Daniele Galiotto - www.g8production.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApplicantViewController : UIViewController
+
+@end
