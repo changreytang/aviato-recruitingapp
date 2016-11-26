27eea8a64633a39971e26ea577a402e5079929f3
Tim Liew <tltimmlw@gmail.com>
2016-11-26 13:50:45 -0800

    Created and implemented view controller for the form to dislay applicant data, still needs to be integrated with segues.

--- iOS/Template Framework Project/Template Framework Project/ApplicantViewController.h
+++ iOS/Template Framework Project/Template Framework Project/ApplicantViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "XLFormViewController.h"
 
-@interface ApplicantViewController : UIViewController
-
+@interface ApplicantViewController : XLFormViewController
 @end
