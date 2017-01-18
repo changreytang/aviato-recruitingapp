12b2843daebeab1791658317ba13f3eb959c3a71
Chang Rey Tang <tang.changrey@gmail.com>
2017-01-18 14:50:09 -0800

    tests

--- iOS/Template Framework Project/Template Framework ProjectTests/Template_Framework_ProjectTests.m
+++ iOS/Template Framework Project/Template Framework ProjectTests/Template_Framework_ProjectTests.m
@@ -8,8 +8,16 @@
 
 #import <XCTest/XCTest.h>
 
-@interface Template_Framework_ProjectTests : XCTestCase
+#import "Applicant.h"
+#import "CropViewController.h"
+#import "ResumeContactParser.h"
 
+@interface Template_Framework_ProjectTests : XCTestCase {
+@private
+    CropViewController *crop_view_controller;
+    Applicant *applicant;
+    ResumeContactParser *resume_contact_parser;
+}
 @end
 
 @implementation Template_Framework_ProjectTests
@@ -24,10 +32,14 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testEmailParsing {
+    NSString* parsed_str = @"Rey Tang tang.changrey@gmail.com (510)283-1574 reytang.me github.com/changreytang linkedin.com/in/changreytang";
+    resume_contact_parser = [[ResumeContactParser alloc] init];
+    XCTAssertEqualObjects([[parser getEmails] componentsJoinedByString:@""], @"tang.changrey@gmail.com", @"Email Parse One Failed");
 }
+
+//[self consoleLog:[numbers componentsJoinedByString:@""]];
+
 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
@@ -35,5 +47,17 @@
         // Put the code you want to measure the time of here.
     }];
 }
+
+//NSString* c_info = [self recognizeImageWithTesseract:image1];
+//NSString* test = @"Rey Tang tang.changrey@gmail.com (510)283-1574 reytang.me github.com/changreytang linkedin.com/in/changreytang";
+//    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"OCR Result"
+//                                                    message:contactInfo
+//                                                   delegate:nil
+//                                          cancelButtonTitle:@"OK"
+//                                          otherButtonTitles:nil];
+//    [alert show];
+//    ResumeContactParser* parser = [[ResumeContactParser alloc] init];
+//    [parser parseContactInfo:test];
+//[self.resumeImageView setImage:image1];
 
 @end
