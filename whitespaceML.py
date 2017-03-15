028df298cd3fa52849983349756cab76c68ae989
andychen152 <andychen152@gmail.com>
2017-03-15 15:53:35 -0700

    Update whitespaceML.py

--- whitespaceML.py
+++ whitespaceML.py
@@ -1,4 +1,3 @@
-
 from PIL import Image
 import os
 import numpy as np
@@ -8,11 +7,11 @@ image = Image.open("/Users/andychen/.Trash/resume_test.jpg") # pulling image
 indexed = np.array(image)                                     # indexed.shape to get array dimen
 
 # training data
-blackTraining = [60,61,62,63,64,65,66,67,120,121,122,123,124,125,131,132,133,134,135,196,284,285,286,287,288,315,316,317,318,319,388,389,390,391,392,497,498,499,500,501]
-whiteTraining = [93,94,95,96,97,98,99,112,113,114,115,116,260,261,262,263,264,265,275,276,277,278,279,280,293,294,295,296,297,298]
+blackTraining = [x.strip() for x in open('blackTraining.txt')] 
+whiteTraining = [x.strip() for x in open('whiteTraining.txt')] 
 # testing data
-blackTesting = [512,513,514,515,516,517,541,542,543,544,545,566,567,568,569,570,571,572]
-whiteTesting = [441,442,443,444,445,446,447,604,605,606,607,608,609,610]
+blackTesting = [x.strip() for x in open('blackTesting.txt')] 
+whiteTesting = [x.strip() for x in oepn('whiteTesting.txt')] 
 
 # function to compute centroid
 def centroidComp(arr):
@@ -33,11 +32,11 @@ def centroidComp(arr):
     return(allSum/len(arr))
    
 
-#blackCentroid = centroidComp(blackTraining)
-#whiteCentroid = centroidComp(whiteTraining)
-#classificationLine = (blackCentroid+whiteCentroid)/2 + 25  # additional 25 to skew it as it works better
+blackCentroid = centroidComp(blackTraining)
+whiteCentroid = centroidComp(whiteTraining)
+classificationLine = (blackCentroid+whiteCentroid)/2 + 25  # additional 25 to skew it as it works better
 
-#print("Classification Line: " + str(classificationLine))
+print("Classification Line: " + str(classificationLine))
 
 # testing on test data
 def testingComp(arr,white):
@@ -50,6 +49,20 @@ def testingComp(arr,white):
             else:
                 lineSum = lineSum + indexed[x][y][0]
         average = lineSum/len(indexed[0])
+        if (white):  # white spot test
+            if (average > classificationLine):
+                print("correct output on line: " + str(x))
+            else:
+                print("WRONG VALUE ON LINE: " +str(x))
+        else:   # black spot test
+            if (average < classificationLine):
+                print("correct output on line: " + str(x) + " val: " + str(average))
+            else:
+                print("WRONG VALUE ON LINE: " +str(x) + " val: " + str(average))
+
+# comment these next 2 lines in real situation. Only a measure of our true positives
+testingComp(whiteTesting,True)
+testingComp(blackTesting,False)
 
 # image splitting
 def isBlack(row):
@@ -74,36 +87,20 @@ secondMargin = 2*margin
 thirdMargin = 3*margin
 fourthMargin = 4*margin
 
-print("Boundary 4 HALT")
-for x in range (0,12,1):
-    print("iterating Boundary 1")
-    print("iterating Boundary 2")
-    print("iterating Boundary 3")
-print("Boundary 2 HALT")
-for x in range (0,5,1):
-    print("iterating Boundary 1")
-    print("iterating Boundary 3")
-print("Boundary 1 HALT")
-for x in range (0,2,1):
-    print("iterating Boundary 3")
-print("Boundary 3 HALT")
-
-
 # original indexes
 #print (firstMargin)
 #print(secondMargin)
 #print(thirdMargin)
 #print(fourthMargin)
 
-
-#while(isBlack(firstMargin)):
-#    firstMargin = firstMargin + 1
-#while(isBlack(secondMargin)):
-#    secondMargin = secondMargin + 1
-#while(isBlack(thirdMargin)):
-#    thirdMargin = thirdMargin + 1
-#while(isBlack(fourthMargin)):
-#    fourthMargin = fourthMargin + 1
+while(isBlack(firstMargin)):
+    firstMargin = firstMargin + 1
+while(isBlack(secondMargin)):
+    secondMargin = secondMargin + 1
+while(isBlack(thirdMargin)):
+    thirdMargin = thirdMargin + 1
+while(isBlack(fourthMargin)):
+    fourthMargin = fourthMargin + 1
 
 # shows indexes of white spaces
 print (firstMargin)
@@ -112,11 +109,11 @@ print(thirdMargin)
 print(fourthMargin)
 
 # the 5 sub images
-np_1 = indexed[:259]
-np_2 = indexed[260:542]
-np_3 = indexed[543:813]
-np_4 = indexed[814:1055]
-np_5 = indexed[1056:]
+np_1 = indexed[:firstMargin]
+np_2 = indexed[firstMargin:secondMargin]
+np_3 = indexed[secondMargin:thirdMargin]
+np_4 = indexed[thirdMargin:fourthMargin]
+np_5 = indexed[fourthMargin:]
 
 # open sub images
 im1 = Image.fromarray(np_1)
@@ -133,7 +130,5 @@ im4.show()
 
 im5 = Image.fromarray(np_5)
 im5.show()
-
-
 
 
